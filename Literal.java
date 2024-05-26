9b81e10046ac655fb1778e5c5294b37152a455f8
Akash120806 <akashind5399@gmail.com>
2024-05-27 00:33:48 +0530

    literals

---
+++ Literal.java
@@ -0,0 +1,13 @@
+class Literal
+{
+    public static void main(String a[])
+    {
+        int num1=100000;
+        int num2=100_00_00;
+        System.out.println(num1);
+        System.out.println(num2);
+
+        
+    }
+}
+    
---
+++ Literal2.java
@@ -0,0 +1,11 @@
+public class Literal2 {
+    public static void main(String a[])
+    {
+        float f=56f;
+       // double f=56;
+        double fo=56.0;
+        System.out.println(f);
+        System.out.println(fo);
+    }
+    
+}
