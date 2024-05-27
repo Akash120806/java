efb394e89b8ad82c5bdc37800c8a645f92c80a54
Akash120806 <akashind5399@gmail.com>
2024-05-27 22:13:31 +0530

    Create Arthimetic.java

---
+++ Arthimetic.java
@@ -0,0 +1,16 @@
+class Arthimetic {
+    public static void main(String[] args) 
+    {
+        int num1=82;
+        int num2=6;
+       int result = num1/num2;//this will give the quotient
+        System.out.println(result);
+
+        int a=82;
+        int b=6;
+       int resul = a%b;//this will give remainder
+        System.out.println(resul);
+        
+        
+    }
+}
