b630214caff5d71035146ace30d30672c8e979b9
Akash120806 <akashind5399@gmail.com>
2024-05-27 22:25:36 +0530

    Create Arthimetic2.java

---
+++ Arthimetic2.java
@@ -0,0 +1,17 @@
+public class Arthimetic2 {
+    public static void main(String []args)
+    {
+        int num1=8;
+      // int num1=num1+1;
+       System.out.println(num1);
+      int result = num1 + 1;
+      System.out.println(result);
+      num1+=1; // we can use othwe arthimetic operators instead of + in that palce
+      System.out.println(num1);
+      num1++;// this means it will add 1 to num1 
+      System.out.println(num1);
+
+    
+    }
+    
+}
