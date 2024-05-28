38a3da39c90513434a2db5dce8e075c035b0759a
Akash120806 <akashind5399@gmail.com>
2024-05-28 22:47:11 +0530

    Create Relationalop2.java

---
+++ Relationalop2.java
@@ -0,0 +1,15 @@
+public class Relationalop2 {
+    public static void main(String args[])
+    {
+        int a= 98;
+        double b= 98.90;
+        int x= 76;
+        double y =798.98;
+        boolean result = a >= b;
+        System.out.println(result);
+        boolean resul= x!=y;
+        System.out.println(resul);
+
+    }
+    
+}
