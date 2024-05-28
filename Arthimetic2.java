2c346ed4bea46599608da774c685e2e1ebb1a069
Akash120806 <akashind5399@gmail.com>
2024-05-28 12:17:53 +0530

    Update Arthimetic2.java

--- Arthimetic2.java
+++ Arthimetic2.java
@@ -6,9 +6,12 @@ public class Arthimetic2 {
        System.out.println(num1);
       int result = num1 + 1;
       System.out.println(result);
-      num1+=1; // we can use othwe arthimetic operators instead of + in that palce
+      num1+=1; // we can use other arthimetic operators instead of + in that palce
       System.out.println(num1);
       num1++;// this means it will add 1 to num1 
+      System.out.println(num1);
+      num1++; //post_increment -|in individual both will give same output
+      ++num1;// pre_increment  -| but in variable output will be different
       System.out.println(num1);
 
     
