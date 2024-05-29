a90df2e1e34230fcf4f046398149ef3ba0184795
Akash120806 <akashind5399@gmail.com>
2024-05-29 21:35:04 +0530

    Create logicalop4.java

---
+++ logicalop4.java
@@ -0,0 +1,19 @@
+public class logicalop4 {
+    public static void main(String[] args) {
+        double x= 89.00;
+        short y=98;
+        byte z=98;
+        int a=78;
+        boolean res= x>=6;{
+            System.out.println( "pass");
+            
+        }
+        boolean re =res || a<=67;
+        System.out.println(!re);
+        System.out.println(re);
+
+
+
+    }
+    
+}
