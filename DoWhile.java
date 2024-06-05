daf8eebafc97dd9165c2ecd37f642452ed29de31
Akash120806 <akashind5399@gmail.com>
2024-06-05 22:14:59 +0530

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,16 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int  x=96;
+        int y=4;
+        do{
+            int j=0;
+            while(j<=2){
+                System.out.println("*");
+                j++;
+
+            }
+
+        }while(x+y<=99);// this statement is false
+    }
+    
+}
