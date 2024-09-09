7cfe9fd5da179908895e42fe3ab3c685d85f0b06
Akash120806 <akashind5399@gmail.com>
2024-09-09 18:59:04 +0530

    modifiers

---
+++ GetterAndSetter.java
@@ -0,0 +1,33 @@
+class GetSet{
+   private  int id;
+     private String name;
+    public String getname(){
+        return name;
+
+    }
+    public void Setname(String na){
+        name=na;
+    }
+    public int getid() {
+        return id;
+
+    }
+    public void Setid(int i){
+        id=i;
+    }
+
+}
+
+
+
+
+public class GetterAndSetter {
+    public static void main(String[] args) {
+        GetSet gs= new GetSet();
+        gs.Setname("Axe");
+        String r=  gs.getname() ;
+        System.out.println(r);
+        gs.Setid(12);
+        System.out.println(gs.getid());
+    }
+}
