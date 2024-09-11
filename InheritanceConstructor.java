2f59b4eab6033697bdd85f0b1cbe80a3f73e247a
Akash120806 <akashind5399@gmail.com>
2024-09-12 01:49:41 +0530

    oops

---
+++ InheritanceConstructor.java
@@ -0,0 +1,33 @@
+
+
+class Cons{
+    Cons(){
+        System.out.println("Make anthoer constructor of different type ");
+    }
+    Cons(int a){
+        System.out.println("This constructor prints"+a);
+    }
+}
+
+
+class Dev extends Cons{
+
+    public Dev() {
+        System.out.println("This is derived constructor");
+    }
+    Dev(int a){
+        System.out.println("This is another constructor which prints "+a);
+    }
+    
+}
+
+public class InheritanceConstructor {
+    public static void main(String[] args) {
+        Dev co= new Dev();
+    }
+    
+   
+    
+    
+    
+}
