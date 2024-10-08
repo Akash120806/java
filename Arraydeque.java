05906078cada85dd413771d5286e4a1a141dfa2f
Akash120806 <akashind5399@gmail.com>
2024-10-08 22:37:01 +0530

    HashSet

--- Arraydeque.java
+++ Arraydeque.java
@@ -3,27 +3,25 @@ import java.util.ArrayDeque;
 public class Arraydeque {
    
     public static void main(String[] args) {
-        // Create an ArrayDeque of Integers
         ArrayDeque<Integer> deque = new ArrayDeque<>();
 
-        // Adding elements to the deque
-        deque.addFirst(10); // Adds 10 to the front
-        deque.addLast(20);  // Adds 20 to the back
-        deque.addLast(30);  // Adds 30 to the back
+        deque.addFirst(10); 
+        deque.addLast(20);  
+        deque.addLast(30);
 
-        // Displaying the deque
-        System.out.println("Deque: " + deque);
+        
+        System.out.println( deque);
 
-        // Accessing elements without removing
-        System.out.println("First element: " + deque.getFirst());  // Retrieves first element
-        System.out.println("Last element: " + deque.getLast());    // Retrieves last element
+        
+        System.out.println( deque.getFirst());  
+        System.out.println( deque.getLast());    
 
-        // Removing elements
+        
         deque.removeFirst();  // Removes the first element
         deque.removeLast();   // Removes the last element
 
-        // Displaying the deque after removal
-        System.out.println("Deque after removal: " + deque);
+       
+        System.out.println(  deque);
     }
 }
 
