f235018e8e23761997b219c75d5bb1ba32bfa997
Akash120806 <akashind5399@gmail.com>
2026-07-25 23:34:30 +0530

    java

---
+++ DataStructre/Recursion/ReverseStack.java
@@ -0,0 +1,24 @@
+
+class ReverseStack {
+    
+    public static void reverseStack(Stack<Integer> st) {
+        if (st.isEmpty()) {
+            return;
+        }
+
+        int top = st.pop();
+        reverseStack(st);
+        insertAtBottom(st, top);
+    }
+
+    public static void insertAtBottom(Stack<Integer> st, int x) {
+        if (st.isEmpty()) {
+            st.push(x);
+            return;
+        }
+
+        int top = st.pop();
+        insertAtBottom(st, x);
+        st.push(top);
+    }
+}
